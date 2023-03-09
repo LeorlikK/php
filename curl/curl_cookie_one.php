a7bf6c7b652a3c276f3d76aaf4bfd38fee6a7239
LeorlikK <leorl1k@yandex.ru>
2023-03-09 23:29:06 +0300

    new

--- curl/curl_cookie_one.php
+++ curl/curl_cookie_one.php
@@ -1,28 +1,17 @@
 <?php
-$website = "https://www.noob-club.ru/";
-$method = "index.php?topic=82966.0";
-$ch = curl_init($website . $method); // запрос
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // данные сохраняются в переменную
-curl_setopt($ch, CURLOPT_HEADER, true); // получает заголовки http пакетов в ту же переменную
-curl_setopt($ch, CURLOPT_NOBODY, true); // убирает тело запроса, оставляя только заголовки
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // переходить по редиректу
-curl_setopt($ch, CURLOPT_PROXY_SSL_VERIFYHOST, false); // отключает проверки httpS
-curl_setopt($ch, CURLOPT_PROXY_SSL_VERIFYPEER, false); // отключает проверки httpS
-$result = curl_exec($ch); // выполнить запрос
-curl_close($ch); // закрытие
 
+//$ch = curl_init('http://localhost:63342/curl/curl_cookie_two.php');
+$ch = curl_init('http://test');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, true);
+
+/** Первый вариант */
+curl_setopt($ch, CURLOPT_COOKIE, 'curl_normal_cookie=111; curl_session_cookie=222');
+/** Второй вариант */
+$cookiefile = 'cookie.txt';
+curl_setopt($ch,CURLOPT_COOKIEJAR, $cookiefile);
+curl_setopt($ch,CURLOPT_COOKIEFILE, $cookiefile);
+//curl_setopt($ch,CURLOPT_COOKIESESSION, $cookiefile); // передает только нормальные куки(не сессионые)
+$result = curl_exec($ch);
+curl_close($ch);
 var_dump($result);
-
-
-
-
-//$file = fopen('pars.html', 'w');
-//fwrite($file, $result);
-////var_dump($result);
-//$needleString = 'Отказ от системы сезонных модификаторов в 10.1 и полный список подземелий 2-го сезона';
-//$result = (string)$result;
-//$len = mb_strlen($needleString);
-//$pos = strpos($result, $needleString);
-//var_dump($len, $pos);
-//$result = substr($result, $pos, $len);
-//var_dump($result);
--- curl/curl_cookie_post.php
+++ curl/curl_cookie_post.php
@@ -1,1 +1,53 @@
 <?php
+session_start();
+$url = "https://www.noob-club.ru/index.php?action=login2";
+$user = 'IgrokRIog';
+$passwrd = 'noodxolidc2023';
+$cookielength = 60;
+$cookieneverexp = true;
+
+$e6776c9cf9 = 'fd8dbf59f6589bad9943cd6fa788c921';
+$hash_passwrd = 0;
+
+function request(string $url, $postData = null, $cookiefile = 'cookie.txt')
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_NOBODY, false);
+
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36');
+
+    curl_setopt($ch, CURLOPT_PROXY_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_PROXY_SSL_VERIFYPEER, false);
+
+    curl_setopt($ch,CURLOPT_COOKIEJAR, $cookiefile);
+    curl_setopt($ch,CURLOPT_COOKIEFILE, $cookiefile);
+
+
+
+    if ($postData){
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    }
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+file_put_contents('cookie.txt', ''); // Обнуляем и убеждаемся, что ничего нет
+
+$postData = [
+    'user' => 'IgrokRIog',
+    'passwrd' => 'noodxolidc2023',
+    'b30dfd20b9' => 'e482e42adcbc8b8aa90ae7c9c549b09f',
+    'hash_passwrd' => ''
+];
+
+$html = request($url, $postData);
+$file = fopen('pars.html', 'w');
+fwrite($file, $html);
+fclose($file);
+echo $html;
+//var_dump($html);
